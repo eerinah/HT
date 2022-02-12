50078f58e07d45c1544a7153f2be547539d8487e
Eerina Haque <eerinahaque@gmail.com>
2022-02-12 11:36:29 -0800

    Moralis config

--- frontend/main.js
+++ frontend/main.js
@@ -4,4 +4,3 @@ const serverUrl = "https://ifry0n1b6gzi.usemoralis.com:2053/server";
 const appId = "ET7KSpOgupHGNVEfzY00jUumM2J38zKdOYtsQsdw";
 Moralis.start({ serverUrl, appId });
 
-/* TODO: Add Moralis Authentication code */
