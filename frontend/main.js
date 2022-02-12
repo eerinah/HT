3a7be2378119f3a7fbc095316ccee34157500353
Eerina Haque <eerinahaque@gmail.com>
2022-02-12 10:58:39 -0800

    Create files for frontend

---
+++ frontend/main.js
@@ -0,0 +1,7 @@
+
+/* Moralis init code */
+const serverUrl = "https://ifry0n1b6gzi.usemoralis.com:2053/server";
+const appId = "ET7KSpOgupHGNVEfzY00jUumM2J38zKdOYtsQsdw";
+Moralis.start({ serverUrl, appId });
+
+/* TODO: Add Moralis Authentication code */
