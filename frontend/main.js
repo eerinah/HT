87bdb8f2e07bef841afdd7a5e2836ff591a3dc04
Eerina Haque <eerinahaque@gmail.com>
2022-02-13 00:27:36 -0800

    Co-authored-by: anaghanair264 <anaghanair264@users.noreply.github.com>

--- frontend/main.js
+++ frontend/main.js
@@ -7,19 +7,25 @@ const TOKEN_CONTRACT_ADDRESS = "0x44e5d0038fad764777C3cAebbcf86D9084188342";
 
 init = async () => {
     hideElement(userInfo);
-    window.web3 = await Moralis.web3.enable();
+    hideElement(userItemsSection);
+    hideElement(createItemForm);
+    const web3Provider = await Moralis.enableWeb3();
+  //  window.tokenContract = new web3Provider.eth.Contract(contractAbi, contractAddress);
     initUser();
+    loadUserItems();
 }
 
 initUser = async () => {
     if (await Moralis.User.current()) { // if user is logged in, show profile not connect
         hideElement(userConnectButton);
         showElement(userProfileButton);
-        console.log("user is logged in");
+        showElement(openCreateItemButton);
+        showElement(openUserItemsButton);
     } else{
         showElement(userConnectButton);
         hideElement(userProfileButton);
-        console.log("user not logged in");
+        hideElement(openCreateItemButton);
+        hideElement(openUserItemsButton);
     }
 }
 
@@ -33,6 +39,7 @@ login = async () => {
 }
 
 logout = async () => {
+    console.log("logging out");
     await Moralis.User.logOut();
     hideElement(userInfo);
     initUser();
@@ -53,9 +60,9 @@ openUserInfo = async () => { // when clicking profile button
         const userAvatar = user.get('avatar');
         if(userAvatar) {
             userAvatarImage.src = userAvatar.url();
-            showElement(userAvatarImg);
+            showElement(userAvatarImage);
         } else{
-            hideElement(userAvatarImg);
+            hideElement(userAvatarImage);
         }
 
         showElement(userInfo);
@@ -66,7 +73,7 @@ openUserInfo = async () => { // when clicking profile button
 
 
 saveUserInfo = async () => {
-    user.set('email', userEmail.value);
+    user.set('email', userEmailField.value);
     user.set('username', userUsernameField.value);
 
     if (userAvatarFile.files.length > 0) { // only takes jpg
@@ -80,15 +87,143 @@ saveUserInfo = async () => {
 }
 
 
+createItem = async () => {
+    console.log("creating item");
+    if (createItemFile.files.length == 0) {
+        alert("Please select a file!");
+        return;
+    } else if (createItemNameField.value.length == 0) {
+        alert("Please give the item a name!");
+        return;
+    }
+
+    const nftFile = new Moralis.File("nftFile.jpg", createItemFile.files[0]);
+
+    // ins try catch block for all promises
+    try {
+        await nftFile.saveIPFS();
+    } catch (error) {
+        console.log("unable to save file");
+    }
+
+
+    const nftFilePath = nftFile.ipfs();
+    const nftFileHash = nftFile.hash();
+
+    const metadata = {
+        name: createItemNameField.value,
+        description: createItemDescriptionField.value,
+        image: nftFilePath,
+    };
+
+    const nftFileMetadataFile  = new Moralis.File("metadata.json", {base64 : btoa(JSON.stringify(metadata))});
+    try {
+        await nftFileMetadataFile.saveIPFS();
+    } catch (error) {
+        console.log("unable to save metadata json");
+    }
+    const nftFileMetadataFilePath = nftFileMetadataFile.ipfs();
+    const nftFileMetadataFileHash = nftFileMetadataFile.hash();
+
+    const nftId = 1;
+    try {
+        const nftId = await mintNft(nftFileMetadataFilePath);
+    } catch (error) {
+        console.log("unable to mint nft");
+    }
+
+
+    const Item = Moralis.Object.extend("Item");
+
+    const item = new Item(); // instance of item
+    item.set('name', createItemNameField.value);
+    item.set('description', createItemDescriptionField.value);
+    item.set('nftFilePath', nftFilePath);
+    item.set('nftFileHash', nftFileHash);
+    item.set('MetadataFilePath', nftFileMetadataFilePath);
+    item.set('MetadataFileHash', nftFileMetadataFileHash);
+    item.set('nftId', nftId);
+    item.set('nftContractAddress', TOKEN_CONTRACT_ADDRESS);
+
+    try {
+        await item.save();
+    } catch (error) {
+        console.log("unable to save item");
+    }
+    console.log(item);
+}
+
+mintNft = async(metadataUri, amount) => {
+    const options = {
+        chain: "ganache",
+        address: TOKEN_CONTRACT_ADDRESS,
+        function_name: "mintCard",
+        abi: tokenContractAbi,
+        params: {uri: metadataUri, fee: amount}
+      };
+    const receipt = await Moralis.Web3API.native.runContractFunction(options);
+    console.log(receipt);
+    return receipt.events.Transfer.returnValues.tokenId;
+}
+
+
+openUserItems = async () => { // when clicking My Items
+  user = await Moralis.User.current();
+  if (user) {
+      showElement(userItemsSection);
+  } else{
+      login();
+  }
+}
+
+loadUserItems = async () => {
+    const ownedItems = await Moralis.Cloud.run("getUserItems");
+    console.log(ownedItems);
+}
+
+
+initTemplate = (id) => {
+    const template = document.getElementById(id);
+    template.id = "";
+    template.parentNode.removeChild(template);
+    return template;
+}
+
+renderUserItem = (item) => {
+  const userItem = userItemTemplate.cloneNode(true);
+  userItem.getElementsByTagName("img")[0].src = item.image;
+  userItem.getElementsByTagName("img")[0].alt = item.name;
+  userItem.getElementsByTagName("h5")[0].innerText = item.name;
+  userItem.getElementsByTagName("p")[0].innerText = item.description;
+
+  userItems.appendChild(item);
+}
+
+getAndRenderItemData = (item, renderFunction) => {
+  fetch(item.tokenUri)
+  .then(response => response.json())
+  .then(data => {
+      
+  })
+}
+
+
 hideElement = (element) => element.style.display = "none";
 showElement = (element) => element.style.display = "block"; //shows element
 
+
+// Navbar
 const userConnectButton = document.getElementById("btnConnect");
-userConnectionButton.onclick = login;
+userConnectButton.onclick = login;
 
 const userProfileButton = document.getElementById("btnUserInfo");
 userProfileButton.onclick = openUserInfo;
 
+const openCreateItemButton = document.getElementById("btnOpenCreateItem");
+openCreateItemButton.onclick = () => showElement(createItemForm);
+
+
+//User Profile
 const userInfo = document.getElementById("userInfo");
 const userUsernameField = document.getElementById("txtUsername");
 const userEmailField = document.getElementById("txtEmail");
@@ -96,6 +231,376 @@ const userAvatarImage = document.getElementById("imgAvatar");
 const userAvatarFile = document.getElementById("fileAvatar");
 
 document.getElementById("btnCloseUserInfo").onclick = () => hideElement(userInfo);
-document.getElementById("btnLogout").onClick = logout;
-document.getElementById("btnSaveUserInfo").onClick = SaveUserInfo;
+document.getElementById("btnLogout").onclick = logout;
+document.getElementById("btnSaveUserInfo").onclick = saveUserInfo;
+
+//Item Creation
+const createItemForm = document.getElementById("createItem");
+
+const createItemNameField = document.getElementById("txtCreateItemName");
+const createItemDescriptionField = document.getElementById("txtCreateItemDescription");
+const createItemPriceField = document.getElementById("numCreateItemPrice");
+const createItemStatusField = document.getElementById("selectCreateItemStatus");
+const createItemFile = document.getElementById("fileCreateItemFile");
+
+document.getElementById("btnCloseCreateItem").onclick = () => hideElement(createItemForm);
+document.getElementById("btnCreateItem").onclick = createItem;
+
+// User items
+const userItemsSection = document.getElementById("userItems");
+const userItems = document.getElementById("userItemsList");
+document.getElementById("btnCloseUserItems").onclick = () => hideElement(userItemsSection);
+const openUserItemsButton = document.getElementById("btnMyItems");
+openUserItemsButton.onclick = openUserItems;
+
+const userItemTemplate = initTemplate("itemTemplate");
+
+var tokenContractAbi = [
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "tokenId",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "askingPrice",
+          "type": "uint256"
+        }
+      ],
+      "name": "cardAdded",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "cardDeposit",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "askingPrice",
+          "type": "uint256"
+        }
+      ],
+      "name": "cardForeclosed",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "buyer",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "askingPrice",
+          "type": "uint256"
+        }
+      ],
+      "name": "cardSold",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "askingPrice",
+          "type": "uint256"
+        }
+      ],
+      "name": "cardUpdatePrice",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "auctionCardList",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "internalType": "address",
+          "name": "tokenAddress",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "tokenId",
+          "type": "uint256"
+        },
+        {
+          "internalType": "address payable",
+          "name": "seller",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "askingPrice",
+          "type": "uint256"
+        },
+        {
+          "internalType": "address payable",
+          "name": "beneficiary",
+          "type": "address"
+        },
+        {
+          "internalType": "bool",
+          "name": "isOwned",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "benefactorFunds",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "benefactors",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "deposit",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "totalOwedTokenCost",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "tokenAddress",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "tokenId",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "askingPrice",
+          "type": "uint256"
+        },
+        {
+          "internalType": "address payable",
+          "name": "beneficiary",
+          "type": "address"
+        }
+      ],
+      "name": "addCardToMarket",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "newAssessedPrice",
+          "type": "uint256"
+        }
+      ],
+      "name": "forceSale",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function",
+      "payable": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        }
+      ],
+      "name": "depositFunds",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "payable",
+      "type": "function",
+      "payable": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        }
+      ],
+      "name": "forecloseCard",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ];
 init();
